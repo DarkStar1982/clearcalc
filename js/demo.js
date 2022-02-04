525506c884b23e34afc2b2d998015bdaa8faefa5
Dennis Silin <d_silin@yahoo.com>
2022-02-04 15:32:58 -0500

    Basic dialog functionality ready

--- js/demo.js
+++ js/demo.js
@@ -1,14 +1,33 @@
-function show_dialog(element_ref)
+function show_dialog(element_ref, dialog_ref, text_message)
 {
-  var id = element_ref.id;
+  var prev_background = document.body.style.background;
+  document.body.style.background = "#7F7F7F";
+  var button_yes = document.getElementsByClassName('button_yes')[0];
+  var button_cancel = document.getElementsByClassName('button_cancel')[0];
+
+  button_yes.addEventListener('click', function() {
+        document.body.style.background = prev_background;
+        dialog.close();
+        update_page(element_ref, text_message+" 'Yes'");
+      });
+  var dialog = dialog_ref;
+
+  button_cancel.addEventListener('click', function() {
+        document.body.style.background = prev_background;
+        dialog.close();
+        update_page(element_ref, text_message+" 'Cancel'");
+      });
   var dialog = document.getElementById('template_dialog');
-  dialog.returnValue = 'favAnimal';
 
   if (typeof dialog.showModal === "function") {
     dialog.showModal();
   } else {
     //-- Safari stub -->
-    // confirm("No, this is "+id);
     alert("The <dialog> API is not supported by this browser");
   }
+}
+
+function update_page(element_ref, text_message)
+{
+    element_ref.innerHTML=text_message;
 }
